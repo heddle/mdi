0220dffbc5b664128017034988922ced2a64efd9
David Heddle <david.heddle@cnu.edu>
2026-02-05 17:28:32 -0500

    removed 3D

--- src/main/java/edu/cnu/mdi/mdi3D/panel/Bad3DPropertyException.java
+++
@@ -1,9 +0,0 @@
-package edu.cnu.mdi.mdi3D.panel;
-
-@SuppressWarnings("serial")
-public class Bad3DPropertyException extends Exception {
-
-	public Bad3DPropertyException(String message) {
-		super(message);
-	}
-}
